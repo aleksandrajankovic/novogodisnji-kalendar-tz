e04e2c0e5ec2963a6c78fbafde028fd8f2e85f0a
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2024-12-16 12:30:39 +0100

    Bug fixed

--- en/lang.js
+++ en/lang.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
   var langLink = document.getElementById("lang-link");
   var flagImg = document.getElementById("flag-img");
 
-  // Funkcija za preusmjeravanje korisnika na stranicu odabranog jezika
+
   function redirectToLanguage(selectedLang) {
     let pathArray = document.URL.split("/");
-    let secondLevelLocation = pathArray[5] || ""; // Ako nema 4. nivoa, postavlja se na prazan string
+    let secondLevelLocation = pathArray[6] || ""; 
     
-    // Formiraj URL na osnovu odabranog jezika
+
     if (selectedLang === "sw") {
       window.location.href = `../${secondLevelLocation}`;
     } else if (selectedLang === "en") {
@@ -18,10 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
- // Funkcija za postavljanje zastave jezika na temelju lokalno pohranjenih podataka
+
  function setLanguageFromLocalStorage() {
   var selectedLang = localStorage.getItem("selectedLang");
-  if (!selectedLang) return; // Ako nema jezika u localStorage, izlazimo
+  if (!selectedLang) return; 
 
   var selectedOption = langDropdown.querySelector(`[data-lang="${selectedLang}"]`);
   if (!selectedOption) {
@@ -33,19 +33,19 @@ document.addEventListener("DOMContentLoaded", function () {
   flagImg.setAttribute("src", selectedImage);
 }
 
-// Provera lokalno pohranjenih podataka i postavljanje zastave na temelju toga
+
 if (langDropdown && localStorage.getItem("selectedLang")) {
   setLanguageFromLocalStorage();
 }
 
-// Dodavanje "click" događaja na zastavu/link
+
 langLink.addEventListener("click", function (event) {
   event.preventDefault();
   langDropdown.style.display =
     langDropdown.style.display === "block" ? "none" : "block";
 });
 
-// Dodavanje "click" događaja na opcije padajućeg menija
+
 var langOptions = langDropdown.querySelectorAll("a[data-lang]");
 langOptions.forEach(function (option) {
   option.addEventListener("click", function (event) {
@@ -53,14 +53,14 @@ langOptions.forEach(function (option) {
     var selectedLang = this.getAttribute("data-lang");
     var selectedImage = this.getAttribute("data-image");
 
-    // Ažuriranje zastave i preusmjeravanje korisnika na odabrani jezik
+
     flagImg.setAttribute("src", selectedImage);
     redirectToLanguage(selectedLang);
 
-    // Spremanje odabranog jezika u localStorage
+
     localStorage.setItem("selectedLang", selectedLang);
 
-    // Sakrij padajući meni nakon što je odabir završen
+
     langDropdown.style.display = "none";
   });
 });
--- lang.js
+++ lang.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
   
     function redirectToLanguage(selectedLang) {
         let pathArray = document.URL.split("/");
-        let secondLevelLocation = pathArray[4] || ""; // Ako nema 4. nivoa, postavlja se na prazan string
+        let secondLevelLocation = pathArray[4] || ""; 
         
-        // Formiraj URL na osnovu odabranog jezika
+
         if (selectedLang === "sw") {
           window.location.href = `./${secondLevelLocation}`;
         } else if (selectedLang === "en") {
@@ -17,10 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       }
   
-    // Funkcija za postavljanje zastave jezika na temelju lokalno pohranjenih podataka
+
     function setLanguageFromLocalStorage() {
       var selectedLang = localStorage.getItem("selectedLang");
-      if (!selectedLang) return; // Ako nema jezika u localStorage, izlazimo
+      if (!selectedLang) return; 
   
       var selectedOption = langDropdown.querySelector(`[data-lang="${selectedLang}"]`);
       if (!selectedOption) {
@@ -32,19 +32,19 @@ document.addEventListener("DOMContentLoaded", function () {
       flagImg.setAttribute("src", selectedImage);
     }
   
-    // Provera lokalno pohranjenih podataka i postavljanje zastave na temelju toga
+   
     if (langDropdown && localStorage.getItem("selectedLang")) {
       setLanguageFromLocalStorage();
     }
   
-    // Dodavanje "click" događaja na zastavu/link
+
     langLink.addEventListener("click", function (event) {
       event.preventDefault();
       langDropdown.style.display =
         langDropdown.style.display === "block" ? "none" : "block";
     });
   
-    // Dodavanje "click" događaja na opcije padajućeg menija
+   
     var langOptions = langDropdown.querySelectorAll("a[data-lang]");
     langOptions.forEach(function (option) {
       option.addEventListener("click", function (event) {
@@ -52,14 +52,14 @@ document.addEventListener("DOMContentLoaded", function () {
         var selectedLang = this.getAttribute("data-lang");
         var selectedImage = this.getAttribute("data-image");
   
-        // Ažuriranje zastave i preusmjeravanje korisnika na odabrani jezik
+    
         flagImg.setAttribute("src", selectedImage);
         redirectToLanguage(selectedLang);
   
-        // Spremanje odabranog jezika u localStorage
+       
         localStorage.setItem("selectedLang", selectedLang);
   
-        // Sakrij padajući meni nakon što je odabir završen
+ 
         langDropdown.style.display = "none";
       });
     });
